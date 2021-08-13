8845e234860f193cd2e01410896e186480cc3055
Alex-cc-Liu <73931476+Alex-cc-Liu@users.noreply.github.com>
2021-08-13 22:56:16 +0800

    Update Server_main.py
    用户登陆的时候会写日志到文件中去
    
    完善了record函数，实验结束的时候可以将数据（误差、路程、速度序列）存储到pickle文件中。同时清空用于记录实验数据的全局变量
    
    增加了测试代码
    
    取消了通过ip地址获取用户登陆的地理信息的函数，搞起来太麻烦。

--- Server_main.py
+++ Server_main.py
@@ -1,18 +1,21 @@
+import pickle
 from flask import Flask, request
-import socket
 import pandas as pd
 import time
 import logging
-import tensorflow
 import keras
+import os
+import socket
 
 app = Flask(__name__)
 
 errors = []
 locations = []
 velocities = []
+# TODO: the file name of the model and how to predict
 model_path = ""
 model = keras.models.load_model(model_path)
+datapath = "data/"
 
 
 @app.route('/SignUp/')
@@ -63,6 +66,8 @@ def LogIn():
     # load the account information from the file
     df = pd.read_csv('user_info.csv', usecols=['username', 'password']).astype(str)
 
+    log_df = pd.read_csv("data/log/log_info.csv")
+
     # find the username and password from the data list
     res = df.query(f"username == '{user}'")
 
@@ -71,7 +76,14 @@ def LogIn():
         logging.info(
             f"{struct_time.tm_year}/{struct_time.tm_mon}/{struct_time.tm_mday}"
             f" {struct_time.tm_hour}:{struct_time.tm_min}:{struct_time.tm_sec} "
-            f"{request.remote_addr} {res.iloc[0]['username']} log in successfully!")
+            f"{ip} {res.iloc[0]['username']} log in successfully!")
+
+        # Write the log information into the og file
+        new_log_df = log_df.append({"date": f"{struct_time.tm_year}/{struct_time.tm_mon}/{struct_time.tm_mday}",
+                                    "time": f"{struct_time.tm_hour}_{struct_time.tm_min}_{struct_time.tm_sec}",
+                                    "username": user,
+                                    "ip": ip})
+        new_log_df.to_csv("data/log_info.csv")
         return "1"
 
     # return the wrong signal
@@ -104,6 +116,29 @@ def record():
     Record the data into the data file.
     :return: None or some signal for check
     """
+    struct_time = time.localtime(time.time())
+    user = request.args.get("username")
+    user_dir = os.path.join(datapath, user)
+    data_dir = os.path.join(user_dir, f"{struct_time.tm_year}_{struct_time.tm_mon}_{struct_time.tm_mday}")
+    data_name = os.path.join(data_dir, f"{struct_time.tm_hour}_{struct_time.tm_min}_{struct_time.tm_sec}.pkl")
+    if not os.path.exists(user_dir):
+        os.makedirs(user_dir)
+
+    if not os.path.exists(data_dir):
+        os.makedirs(data_dir)
+
+    with open(data_name, "wb") as file:
+        pickle.dump({"Errors": errors,
+                     "Locations": locations,
+                     "Velocities": velocities}, file)
+    logging.info(f"{data_name} done!")
+    
+    # clear the list of the data.
+    errors.clear()
+    locations.clear()
+    velocities.clear()
+    
+    return "1"
 
 
 @app.route("/check/")
@@ -115,4 +150,23 @@ def checkIP():
     return "LHDL_YYDS_2021"
 
 
-def getIPAddress(ip):
+def get_host_ip():
+    """
+    Get the IP address of the local host
+    :return: IP address for the local host
+    """
+    try:
+        s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+        s.connect(('8.8.8.8', 80))
+        ip = s.getsockname()[0]
+    finally:
+        s.close()
+
+    return ip
+
+
+if __name__ == "__main__":
+    """Run the Server"""
+    local_IP = get_host_ip()
+    print(local_IP)
+    app.run(host=local_IP)
